c27c60ab7d4aa450fb94228840cad3069475b091
Sshivani48 <133041502+Sshivani48@users.noreply.github.com>
2023-05-15 17:30:54 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,9 @@
+class Narrowing {
+    public static void main(String[] args) {
+        System.out.println("Explicit/Narrowing type");
+        char s='.';
+        short w=(short)s;
+        System.out.println(s);
+        System.out.println(w);
+    }
+}
---
+++ NprimitiveDTex.java
@@ -0,0 +1,8 @@
+public class NprimitiveDTex {
+
+  public static void main(String[] args) {
+    String str1 =new String("Non-Primitive");
+    String str2 =new String("Data-Type");
+    System.out.print(str1 + " " + str2);
+  }
+}
